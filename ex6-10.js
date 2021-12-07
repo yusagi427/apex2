299593250dbcd614d3075c029499ef2eab35aa67
yusagi427 <zm19570@josai.ac.jp>
2021-12-07 01:59:41 +0000

    1207

---
+++ ex6-10.js
@@ -0,0 +1,8 @@
+function imgchange1(){
+    document.getElementById('logo').src = '/image/txt2.png';
+}
+
+function imgchange2(){
+    document.getElementById('logo').src = '/image/Josai Logo.jpg';
+}
+
