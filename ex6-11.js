b5d4d6c35631dd23845df747eeec0dd816f90b91
yusagi427 <zm19570@josai.ac.jp>
2021-12-14 01:59:19 +0000

    1214

--- ex6-11.js
+++ ex6-11.js
@@ -20,3 +20,13 @@ function startfnc(){
 function mes(){
     alert("3秒経ちました！");
 }
+
+function hyoji(){
+    //表示する文字
+    var str = "一文字ずつ表示します。";
+    //テキストボックスの文字数    
+    var cnt = document.timer.moji.value.length;
+    //文字が全部表示されているか確認 if (cnt<11){
+    //現在より一文字多く切り出して表示
+    document.timer.moji.value = 
+}
