dce0789a4ae49bb77a1d8bc40bb82b6d3557ccc9
yusagi427 <zm19570@josai.ac.jp>
2022-01-18 01:50:32 +0000

    0118

--- ex6-12.js
+++ ex6-12.js
@@ -6,5 +6,7 @@ function hyoji(){
     //文字が全部表示されているか確認 if (cnt<11){
     //現在より一文字多く切り出して表示
     document.timer.moji.value = str.substr(0,cnt+1);}else{
-        //すべて表示されたら、空文字に戻す
-    }
+    //すべて表示されたら、空文字に戻す
+    document.timer.moji.value="";}
+    //setTimeout()を含む関数を呼び出す
+    setTimeout("hyoji()",1000);}
